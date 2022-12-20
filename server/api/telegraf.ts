173a1041fb4839f285020a965118956e47eb3247
_____W_somnium____ <56722571+HideTheTime@users.noreply.github.com>
2022-12-20 21:08:37 +0800

    Update telegraf.ts
    更改文件 让dev一开始就启动

--- server/api/telegraf.ts
+++ server/api/telegraf.ts
@@ -1,14 +1,14 @@
 import { Telegraf } from "telegraf";
 import { botApiKey } from "../config";
 // import { message } from "telegraf/filters";
-
+let bot = new Telegraf(botApiKey);
+bot.command("oldschool", (ctx) => ctx.reply("成功了"));
+bot.command("hello", (ctx) => ctx.reply("成功了不用测试了"));
+bot.command("test", Telegraf.reply("λ"));
+bot.launch();
 export default defineEventHandler(async (e) => {
 	// let date = await Date.now();
-	let bot = new Telegraf(botApiKey);
-	bot.command("oldschool", (ctx) => ctx.reply("成功了"));
-	bot.command("hello", (ctx) => ctx.reply("成功了不用测试了"));
-	bot.command("test", Telegraf.reply("λ"));
-	bot.launch();
+
 	return {
 		dateTime: Date.now(),
 		hello: botApiKey,
