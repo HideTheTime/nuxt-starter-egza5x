2bcffc9e46bdef6add8cd369083b5c99f535ac0e
_____W_somnium____ <56722571+HideTheTime@users.noreply.github.com>
2022-12-20 23:10:08 +0800

    123

---
+++ server/api/telegraf.post.ts
@@ -0,0 +1,19 @@
+import { Telegraf } from "telegraf";
+import { botApiKey } from "../config";
+// import { message } from "telegraf/filters";
+let bot = new Telegraf(botApiKey);
+bot.command("oldschool", (ctx) => ctx.reply("成功了"));
+bot.command("hello", (ctx) => ctx.reply("成功了不用测试了1"));
+bot.command("test", Telegraf.reply("λ"));
+bot.launch();
+export default defineEventHandler(async (e) => {
+	// let date = await Date.now();
+
+    const body = useBody(e)
+    console.log(body)
+	return {
+		dateTime: Date.now(),
+		hello: botApiKey,
+        body
+	};
+});
--- server/api/telegraf.ts
+++ server/api/telegraf.ts
@@ -1,11 +1,11 @@
-import { Telegraf } from "telegraf";
-import { botApiKey } from "../config";
-// import { message } from "telegraf/filters";
-let bot = new Telegraf(botApiKey);
-bot.command("oldschool", (ctx) => ctx.reply("成功了"));
-bot.command("hello", (ctx) => ctx.reply("成功了不用测试了1"));
-bot.command("test", Telegraf.reply("λ"));
-bot.launch();
+// import { Telegraf } from "telegraf";
+// import { botApiKey } from "../config";
+// // import { message } from "telegraf/filters";
+// let bot = new Telegraf(botApiKey);
+// bot.command("oldschool", (ctx) => ctx.reply("成功了"));
+// bot.command("hello", (ctx) => ctx.reply("成功了不用测试了1"));
+// bot.command("test", Telegraf.reply("λ"));
+// bot.launch();
 export default defineEventHandler(async (e) => {
 	// let date = await Date.now();
 
